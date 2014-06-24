d4fd0175a24edc4820399dd1321fcc9f8d0148be
Jamie Booth <JamieB226@aol.com>
2014-06-24 16:30:37 -0400

    Convert BT code to BTLE
    BT code did not work because the watch uses BTLE which is a different
    protocol.  Rewrite of connection code in progress.

--- Android/src/com/boothcomputing/arduinowatch/ArduinoWatchService.java
+++ Android/src/com/boothcomputing/arduinowatch/ArduinoWatchService.java
@@ -3,17 +3,27 @@ package com.boothcomputing.arduinowatch;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import java.util.TimeZone;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.UUID;
 
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothAdapter.LeScanCallback;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
@@ -37,36 +47,49 @@ public class ArduinoWatchService extends Service
 	public static int intGmailMessages;
 	public static int intIMAPMessages;
 	public static int intRSSUnread;
-	byte[] watchBuffer=new byte[32];
-	byte[] outBuffer=new byte[32];
-	private OutputStream outputStream;
-	private InputStream inStream;
+	byte[] watchBuffer = new byte[32];
+	byte[] outBuffer = new byte[32];
 	private static boolean blnSync;
-	BluetoothSocket socket;
-	
+
+	// UUIDs for UAT service and associated characteristics.
+	public static UUID UART_UUID = UUID
+			.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+	public static UUID TX_UUID = UUID
+			.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+	public static UUID RX_UUID = UUID
+			.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+	// UUID for the BTLE client characteristic which is necessary for
+	// notifications.
+	public static UUID CLIENT_UUID = UUID
+			.fromString("00002902-0000-1000-8000-00805f9b34fb");
+
+	// BTLE state
+	private BluetoothAdapter adapter;
+	private BluetoothGatt gatt;
+	private BluetoothGattCharacteristic tx;
+	private BluetoothGattCharacteristic rx;
+
 	final Handler timerHandler = new Handler();
-	Runnable timerRunnable = new Runnable() {
+	Runnable timerRunnable = new Runnable()
+	{
 
-	        @Override
-	        public void run() 
-	        {
-	     	   if(socket != null)
-	     	   {
-		     	   if(socket.isConnected() == true)
-		     	   {
-			     	   sendTimeUpdate();
-			     	   sendNotificationUpdate();
-		     	   }
-	     	   }
+		@Override public void run()
+		{
 
-	            timerHandler.postDelayed(this, 5000);
-	        }
-	    };
+			if(tx != null)
+			{
+				sendTimeUpdate();
+				sendNotificationUpdate();
+			}
+
+			timerHandler.postDelayed(this, 5000);
+		}
+	};
 
 	public ArduinoWatchService()
 	{
 		super();
-		
+
 		System.out.println("Service:  Created Service");
 		// TODO Auto-generated constructor stub
 	}
@@ -74,12 +97,12 @@ public class ArduinoWatchService extends Service
 	protected void onHandleIntent(Intent intent)
 	{
 		System.out.println("Service:  Processing intent");
-		
-		switch(intent.getAction())
+
+		switch (intent.getAction())
 		{
 		case "StartSync":
 			System.out.println("Service:  StartSync");
-			blnSync=true;
+			blnSync = true;
 			break;
 		case "ReadSettings":
 			System.out.println("Service: ReadSettings");
@@ -87,19 +110,20 @@ public class ArduinoWatchService extends Service
 			break;
 		case "StopSync":
 			System.out.println("Service:  StopSync");
-			blnSync=false;
+			blnSync = false;
 			break;
 		default:
-			System.out.println("Service:  Received unknown Intent Action:"+intent.getAction());
+			System.out.println("Service:  Received unknown Intent Action:"
+					+ intent.getAction());
 			break;
 		}
 	}
 
 	public void main(String[] args)
-	{		
-		
+	{
+
 	}
-	
+
 	private void sendTimeUpdate()
 	{
 		Calendar cal = Calendar.getInstance();
@@ -125,7 +149,8 @@ public class ArduinoWatchService extends Service
 
 	private void readSettings()
 	{
-		SharedPreferences settings = getBaseContext().getSharedPreferences("ArduinoWatch", Context.MODE_PRIVATE);
+		SharedPreferences settings = getBaseContext().getSharedPreferences(
+				"ArduinoWatch", Context.MODE_PRIVATE);
 		strRSSFeedURL = settings.getString("RSSFeedURL", "");
 		strRSSFeedUserName = settings.getString("RSSFeedUserName", "");
 		strRSSFeedPassword = settings.getString("RSSFeedPassword", "");
@@ -138,48 +163,18 @@ public class ArduinoWatchService extends Service
 
 	private void initWatchConnection() throws IOException
 	{
-		BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (blueAdapter != null)
+		adapter = BluetoothAdapter.getDefaultAdapter();
+		System.out.println("Scanning for devices...");
+		if (adapter != null)
 		{
 			System.out.println("Service:  Got Default Adapter");
-			if (blueAdapter.isEnabled())
+			if (adapter.isEnabled())
 			{
 				System.out.println("Service:  Bluetooth is Enabled");
-				if(BluetoothAdapter.checkBluetoothAddress(strWatchBTID) == true)
+				if (BluetoothAdapter.checkBluetoothAddress(strWatchBTID) == true)
 				{
 					System.out.println("Service:  Watch Bluetooth Address is valid.");
-					BluetoothDevice bdWatch = blueAdapter.getRemoteDevice(strWatchBTID.toUpperCase());
-					if(bdWatch != null)
-					{
-						System.out.println("Service:  Got Remote Device for Watch");
-						ParcelUuid[] uuids = bdWatch.getUuids();
-						if(uuids != null)
-						{
-							System.out.println("Service:  Got uuids for Watch");
-							if(uuids.length > 0)
-							{
-								socket = bdWatch.createRfcommSocketToServiceRecord(uuids[0].getUuid());
-								if(socket != null)
-								{
-									System.out.println("Service:  Got socket for Watch");
-									socket.connect();
-								}
-							}
-						}
-					}
-					if(socket != null)
-					{
-						if(socket.isConnected() == true)
-						{
-							System.out.println("Service:  Connected to watch");
-							outputStream = socket.getOutputStream();
-							inStream = socket.getInputStream();
-						}
-						else
-						{
-							System.out.println("Service:  Failed to connect to watch.");
-						}
-					}
+					adapter.startLeScan(scanCallback);
 				}
 				else
 				{
@@ -195,55 +190,21 @@ public class ArduinoWatchService extends Service
 
 	public void sendToWatch(String s) throws IOException
 	{
-		try
+		if (tx == null)
 		{
-			outputStream.write(s.getBytes());
+			// Do nothing if there is no device or message to send.
+			return;
 		}
-		catch (IOException e)
+		// Update TX characteristic value. Note the setValue overload that
+		// takes a byte array must be used.
+		tx.setValue(s.getBytes());
+		if (gatt.writeCharacteristic(tx))
 		{
-			throw e;
+			System.out.println("Sent: " + s.getBytes());
 		}
-	}
-	
-	public void readFromWatch() throws IOException
-	{
-		int bytesRead;
-		
-		try
+		else
 		{
-			// Try reading from watch buffer
-			bytesRead=inStream.read(watchBuffer, 0, 32);
-			
-			//check to see if we got data.  If so, process it.
-			if(bytesRead != -1)
-			{
-				processWatchString(new String(watchBuffer,0,bytesRead));
-			}
-		}
-		catch (IOException e)
-		{
-			throw e;
-		}
-	}
-
-	public void processWatchString(String receivedString)
-	{
-		String[] commands;
-		
-		commands=receivedString.split("$");
-		
-		for(String command : commands)
-		{
-			if(command.startsWith("$tm"))
-			{
-				//Send time update
-				sendTimeUpdate();
-			}
-			if(command.startsWith("$nf"))
-			{
-				//Send Notification update
-				sendNotificationUpdate();
-			}
+			System.out.println("Couldn't write TX characteristic!");
 		}
 	}
 
@@ -251,12 +212,19 @@ public class ArduinoWatchService extends Service
 	{
 		try
 		{
-			sendToWatch("$nf"+Integer.toString(intGmailMessages)+","+Integer.toString(intIMAPMessages)+","+Integer.toString(intRSSUnread));
+			sendToWatch("$nf" + Integer.toString(intGmailMessages) + ","
+					+ Integer.toString(intIMAPMessages) + ","
+					+ Integer.toString(intRSSUnread));
 		}
 		catch (IOException e)
 		{
 			System.out.println("Connection to watch broken.  Command not sent:"
-					+ "$nf"+Integer.toString(intGmailMessages)+","+Integer.toString(intIMAPMessages)+","+Integer.toString(intRSSUnread));
+					+ "$nf"
+					+ Integer.toString(intGmailMessages)
+					+ ","
+					+ Integer.toString(intIMAPMessages)
+					+ ","
+					+ Integer.toString(intRSSUnread));
 		}
 	}
 
@@ -265,67 +233,215 @@ public class ArduinoWatchService extends Service
 		return null;
 	}
 
-	@Override
-	  public void onCreate()
+	@Override public void onCreate()
 	{
 		System.out.println("Service:  Created");
 	}
-	
-	@Override
-	  public void onDestroy() {
+
+	@Override public void onDestroy()
+	{
 		System.out.println("Service:  Destroyed");
 	}
-	
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId)
+
+	@Override public int onStartCommand(Intent intent, int flags, int startId)
 	{
 		readSettings();
-		final Runnable r = new Runnable() 
+		try
 		{
-			public void run() 
+			initWatchConnection();
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error Connecting");
+		}
+		final Runnable r = new Runnable()
+		{
+			public void run()
 			{
 				timerHandler.postDelayed(timerRunnable, 5000);
-				while(true)
+				while (true)
 				{
-					try
-					{
-						if(socket != null)
-						{
-							if(socket.isConnected() != true)
-							{
-								initWatchConnection();
-							}
-						}
-						else
-						{
-							initWatchConnection();
-						}
-					}
-					catch (IOException e)
-					{
-						System.out.println("Unable to connect to watch.");
-					}
-					
-					try
-					{
-						if(socket != null)
-						{
-							if(inStream != null)
-							{
-								readFromWatch();
-							}
-						}
-					}
-					catch (IOException e)
-					{
-						System.out.println("Connection to watch broken.  Unable to read from watch.");
-					}
-					
+					// do stuff
 				}
 			}
 		};
 		Thread t = new Thread(r);
 		t.start();
 		return Service.START_STICKY;
+	}
+
+	private BluetoothGattCallback callback = new BluetoothGattCallback()
+	{
+		// Called whenever the device connection state changes, i.e. from
+		// disconnected to connected.
+		@Override public void onConnectionStateChange(BluetoothGatt gatt,
+				int status, int newState)
+		{
+			super.onConnectionStateChange(gatt, status, newState);
+			if (newState == BluetoothGatt.STATE_CONNECTED)
+			{
+				System.out.println("Connected!");
+				// Discover services.
+				if (!gatt.discoverServices())
+				{
+					System.out.println("Failed to start discovering services!");
+				}
+			}
+			else if (newState == BluetoothGatt.STATE_DISCONNECTED)
+			{
+				System.out.println("Disconnected!");
+			}
+			else
+			{
+				System.out.println("Connection state changed. New state: "
+						+ newState);
+			}
+		}
+
+		// Called when services have been discovered on the remote device.
+		// It seems to be necessary to wait for this discovery to occur before
+		// manipulating any services or characteristics.
+		@Override public void onServicesDiscovered(BluetoothGatt gatt,
+				int status)
+		{
+			super.onServicesDiscovered(gatt, status);
+			if (status == BluetoothGatt.GATT_SUCCESS)
+			{
+				System.out.println("Service discovery completed!");
+			}
+			else
+			{
+				System.out.println("Service discovery failed with status: "
+						+ status);
+			}
+			// Save reference to each characteristic.
+			tx = gatt.getService(UART_UUID).getCharacteristic(TX_UUID);
+			rx = gatt.getService(UART_UUID).getCharacteristic(RX_UUID);
+			// Setup notifications on RX characteristic changes (i.e. data
+			// received).
+			// First call setCharacteristicNotification to enable
+			// notification.
+			if (!gatt.setCharacteristicNotification(rx, true))
+			{
+				System.out.println("Couldn't set notifications for RX characteristic!");
+			}
+			// Next update the RX characteristic's client descriptor to
+			// enable notifications.
+			if (rx.getDescriptor(CLIENT_UUID) != null)
+			{
+				BluetoothGattDescriptor desc = rx
+						.getDescriptor(CLIENT_UUID);
+				desc.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+				if (!gatt.writeDescriptor(desc))
+				{
+					System.out.println("Couldn't write RX client descriptor value!");
+				}
+			}
+			else
+			{
+				System.out.println("Couldn't get RX client descriptor!");
+			}
+		}
+
+		// Called when a remote characteristic changes (like the RX
+		// characteristic).
+		@Override public void onCharacteristicChanged(BluetoothGatt gatt,
+				BluetoothGattCharacteristic characteristic)
+		{
+			super.onCharacteristicChanged(gatt, characteristic);
+			System.out.println("Received: "
+					+ characteristic.getStringValue(0));
+		}
+	};
+
+	// BTLE device scanning callback.
+	private LeScanCallback scanCallback = new LeScanCallback()
+	{
+		// Called when a device is found.
+		@Override public void onLeScan(BluetoothDevice bluetoothDevice,
+				int i, byte[] bytes)
+		{
+			System.out.println("Found device: "
+					+ bluetoothDevice.getAddress());
+			if (bluetoothDevice.getAddress() == strWatchBTID)
+			{
+				// Check if the device has the UART service.
+				if (parseUUIDs(bytes).contains(UART_UUID))
+				{
+					// Found a device, stop the scan.
+					adapter.stopLeScan(scanCallback);
+					System.out.println("Found UART service!");
+					// Connect to the device.
+					// Control flow will now go to the callback functions
+					// when
+					// BTLE events occur.
+					gatt = bluetoothDevice.connectGatt(
+							getApplicationContext(), false, callback);
+				}
+			}
+		}
+	};
+
+	private List<UUID> parseUUIDs(final byte[] advertisedData)
+	{
+		List<UUID> uuids = new ArrayList<UUID>();
+
+		int offset = 0;
+		while (offset < (advertisedData.length - 2))
+		{
+			int len = advertisedData[offset++];
+			if (len == 0)
+				break;
+
+			int type = advertisedData[offset++];
+			switch (type)
+			{
+			case 0x02: // Partial list of 16-bit UUIDs
+			case 0x03: // Complete list of 16-bit UUIDs
+				while (len > 1)
+				{
+					int uuid16 = advertisedData[offset++];
+					uuid16 += (advertisedData[offset++] << 8);
+					len -= 2;
+					uuids.add(UUID.fromString(String.format(
+							"%08x-0000-1000-8000-00805f9b34fb", uuid16)));
+				}
+				break;
+			case 0x06:// Partial list of 128-bit UUIDs
+			case 0x07:// Complete list of 128-bit UUIDs
+				// Loop through the advertised 128-bit UUID's.
+				while (len >= 16)
+				{
+					try
+					{
+						// Wrap the advertised bits and order them.
+						ByteBuffer buffer = ByteBuffer.wrap(
+								advertisedData, offset++, 16).order(
+								ByteOrder.LITTLE_ENDIAN);
+						long mostSignificantBit = buffer.getLong();
+						long leastSignificantBit = buffer.getLong();
+						uuids.add(new UUID(leastSignificantBit,
+								mostSignificantBit));
+					}
+					catch (IndexOutOfBoundsException e)
+					{
+						// Defensive programming.
+						// Log.e(LOG_TAG, e.toString());
+						continue;
+					}
+					finally
+					{
+						// Move the offset to read the next uuid.
+						offset += 15;
+						len -= 16;
+					}
+				}
+				break;
+			default:
+				offset += (len - 1);
+				break;
+			}
+		}
+		return uuids;
 	}
 }
